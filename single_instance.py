485b8fecf7a24e907e37700a3347186607a2a5f3
ubuntu16.04 <huangkai919@163.com>
2017-07-31 14:42:03 +0800

    add sg method

--- single_instance.py
+++ single_instance.py
@@ -13,7 +13,24 @@ class MyClass(object):
     def __init__(self, x):
         self.x = x
 
+
+class SingletonClass(object):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super(SingletonClass, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+class MyClass2(SingletonClass):
+    def __init__(self, x):
+        self.x = x
+
+
 if __name__ == '__main__':
     c1 = MyClass(1)
     c2 = MyClass(2)
+    print 'c1 is c2: {}'.format(c1 is c2)
+
+    c1 = MyClass2(1)
+    c2 = MyClass2(2)
     print 'c1 is c2: {}'.format(c1 is c2)
