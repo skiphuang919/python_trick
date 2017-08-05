f333e795055cac66857ef37f0fe20525110e2f5a
huangkai <huangkai919@163.com>
2017-08-05 13:20:18 +0800

    add multi_inher

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ LEGI原则： `locals -> enclosing function -> globals -> __builtins__`
 * 协程用的是gevent库
 * 简单测试同一个任务用三种不同的形式执行效率
 * monkey的patch对线程池的使用有影响，所以分开执行
+
+## python 多继承
+* 当类是经典类的时候，按照深度优先查找属性或方法
+* 当类是新式类的时候，按照广度优先查找属性或方法
---
+++ multi_inherit.py
@@ -0,0 +1,70 @@
+class A:
+
+    def bar(self):
+        print 'A.bar'
+
+    def foo(self):
+        print 'A.foo'
+
+
+class B(A):
+
+    def bar(self):
+        print 'B.bar'
+
+
+class C(A):
+
+    def bar(self):
+        print 'C.bar'
+
+    def foo(self):
+        print 'C.foo'
+
+
+class D(B, C):
+    pass
+
+
+class NewBase(object):
+
+    def bar(self):
+        print 'Base.bar'
+
+    def foo(self):
+        print 'Base.foo'
+
+
+class New1(NewBase):
+
+    def bar(self):
+        print 'New1.bar'
+
+
+class New2(NewBase):
+
+    def bar(self):
+        print 'New2.bar'
+
+    def foo(self):
+        print 'New2.foo'
+
+
+class New3(New1, New2):
+    pass
+
+
+if __name__ == '__main__':
+    print 'old class:'
+    # DFS
+    # search order: D -> B -> A -> C
+    d = D()
+    d.bar()
+    d.foo()
+    print 'new class:'
+    # BFS
+    # search order New3 -> New1 -> New2 -> BaseNew
+    n = New3()
+    n.bar()
+    n.foo()
+
