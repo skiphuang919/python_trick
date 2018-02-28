67a1200904b4ff413fb1c8ae5378203dd7b0f8a8
skiphuang919 <huangkai919@163.com>
2018-02-28 20:03:42 +0800

    Update ele2.py
    finished

--- ele2.py
+++ ele2.py
@@ -1,49 +1,99 @@
+class Container:
+    def __init__(self, array):
+        self.c_list = array
+        self.a_len = len(self.c_list)
 
-a = [5, 4, 2, 1, 2, 3, 2, 1, 0, 1, 2, 4]
+        self.con_list = []
 
-x = 1
-m = 100
+        self._list = self.c_list[:]
+        self.show()
 
-a_len = len(a)
-max_i = max(a)
+    @staticmethod
+    def is_ordered(list_):
+        return True if sorted(list_, reverse=True) == list_ else False
 
-container = []
-for row_num in list(range(1, max_i+1))[::-1]:
-    row = []
-    for item in a:
-        if item >= row_num:
-            row.append('#')
+    @staticmethod
+    def get_fall_index(list_, index, v):
+        tmp_list = list_[index:]
+        for i_ in range(len(tmp_list)):
+            if tmp_list[i_] > v:
+                return None
+            if tmp_list[i_] == v:
+                continue
+            if tmp_list[i_] < v:
+                return index + i_
+
+    @staticmethod
+    def _gen_container_list(data_list):
+        max_i = max(data_list)
+        res = []
+        for row_num in list(range(1, max_i + 1))[::-1]:
+            row = []
+            for item in data_list:
+                if item >= row_num:
+                    row.append('#')
+                else:
+                    row.append('_')
+            res.append(row)
+        return res
+
+    def flu(self, en_index):
+        if en_index in (0, len(a) - 1) or self.is_ordered(self._list):
+            return
         else:
-            row.append('_')
-    container.append(row)
+            left = self._list[en_index - 1]
+            right = self._list[en_index + 1]
+            enter = self._list[en_index]
+
+            if enter < left and enter < right:
+                self._list[en_index] += 1
+                return
+
+            if enter == right < left:
+                fall_index = self.get_fall_index(self._list, en_index, enter)
+                if fall_index is not None:
+                    self.flu(fall_index)
+                else:
+                    self._list[en_index] += 1
+
+            if (right < enter < left) or (enter == left > right):
+                self.flu(en_index + 1)
+            if (left == enter < right) or (left < enter == right) or \
+                    (left < enter < right) or (enter > left and enter > right):
+                self.flu(en_index - 1)
+            if left == right == enter:
+                self.flu(en_index - 1)
+
+    def show(self):
+        self.con_list = self._gen_container_list(self.c_list)
+        print('The container shape...')
+        for r in self.con_list:
+            print(''.join(r))
+
+    def drop_waters(self, drop_point, waters):
+        print('--drop {} waters in {}--'.format(waters, drop_point))
+
+        if drop_point > len(self.c_list):
+            print('Error: drop point out of container...')
+            return
+
+        for _ in range(waters):
+            self.flu(drop_point)
+        con_list_with_wat = self._gen_container_list(self._list)
+
+        for row_num in range(len(con_list_with_wat)):
+            for item_index in range(len(con_list_with_wat[row_num])):
+                if con_list_with_wat[row_num][item_index] != self.con_list[row_num][item_index]:
+                    con_list_with_wat[row_num][item_index] = 'W'
+
+        for _ in con_list_with_wat:
+            print(''.join(_))
 
 
-def flu(c_list, en_index, drop):
-    left = c_list[en_index-1]
-    right = c_list[en_index+1]
-    enter = c_list[en_index]
-
-    if drop == 0:
-        return
-
-    if enter < right and enter < left:
-        c_list[en_index] += 1
-        drop -= 1
-    if enter > right and enter > left:
-        flu(c_list, en_index-1, drop)
-    if left <= enter < right:
-        flu(c_list, en_index-1, drop)
-    if left > enter >= right:
-        flu(c_list, en_index+1, drop)
-
-
-print(a)
-
-for i in range(8):
-
-    flu(a, 5, 1)
-    print(a)
-
-
-for r in container:
-    print(''.join(r))
+if __name__ == '__main__':
+    a = [5, 4, 2, 1, 2, 3, 2, 1, 0, 1, 2, 4]
+    c = Container(a)
+    print('=' * 30)
+    c.drop_waters(100, 8)
+    print('=' * 30)
+    c.drop_waters(1, 100)
